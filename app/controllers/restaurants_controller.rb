eacdd22e68ac59ce275d326cd5fca1bea9ce75cc
Dan Engel <dengel29@gmail.com>
2017-12-25 14:37:47 +0800

    trash

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,65 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    set_restaurant
+  end
+
+  def update
+    set_restaurant
+    respond_to do |format|
+      if @restaurant.update(restaurant_params)
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
+        format.json { render :show, status: :ok, location: @restaurant }
+      else
+        format.html { render :edit }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    set_restaurant
+    @restaurant.destroy
+  end
+
+  private
+
+  def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone, :category)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone:        "2012140220",
+    category:     "french",
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone:        "2016707797",
+    category:     "english",
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone:        "8675309999",
+    category:     "chinese",
+  }
+]
+Restaurant.create!(restaurants_attributes)
